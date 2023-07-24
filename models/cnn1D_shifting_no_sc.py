6db20ae9dd971a58d8395e1c98384e05598675e0
Jimeng Shi <60025043+JimengShi@users.noreply.github.com>
2023-07-24 14:33:18 -0400

    Add files via upload

---
+++ models/cnn1D_shifting_no_sc.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+
+import os
+import sys
+import pandas as pd
+from math import sqrt
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import Model, Input, layers
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import LSTM, SimpleRNN, GRU
+from tensorflow.keras.layers import Dense, Dropout, concatenate
+
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import mean_absolute_error as mae
+
+
+# adding data_processing to the system path
+sys.path.insert(0, '/aul/homes/jshi008/IvyProjects/SimDL/data_processing')
+from preprocessing import ws_preprocessing, pm25_preprocessing, ele_preprocessing
+
+
+# set gpu
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
+
+
+# get the preprocessed train_X, train_y, test_X, test_y from the dataset
+n_hours = 72
+K = 24
+S = 24
+
+
+### pick one of datasets
+# train_X, train_y, test_X, test_y, scaler = ws_preprocessing(n_hours, K, S)
+# train_X, train_y, test_X, test_y, scaler = pm25_preprocessing(n_hours, K)
+train_X, train_y, test_X, test_y, scaler = ele_preprocessing(n_hours, K)
+
+
+# build model
+model_input = Input(shape=(train_X.shape[1], train_X.shape[2]))
+
+# 4 cnn with skip connection
+x = layers.Conv1D(filters=256, 
+                    kernel_size=2, 
+                    activation='relu', 
+                    padding='same', 
+                    kernel_regularizer=keras.regularizers.L1L2(l1=1e-5, l2=1e-5))(model_input)
+
+x = layers.Conv1D(filters=128, 
+                    kernel_size=2, 
+                    activation='relu', 
+                    padding='same', 
+                    kernel_regularizer=keras.regularizers.L1L2(l1=1e-5, l2=1e-5))(x)
+
+x = layers.Conv1D(filters=64, 
+                    kernel_size=2, 
+                    activation='relu', 
+                    padding='same', 
+                    kernel_regularizer=keras.regularizers.L1L2(l1=1e-5, l2=1e-5))(x)
+
+x = layers.Conv1D(filters=32, 
+                    kernel_size=2, 
+                    activation='relu', 
+                    padding='same', 
+                    kernel_regularizer=keras.regularizers.L1L2(l1=1e-5, l2=1e-5))(x)
+
+x = layers.Flatten()(x)
+
+
+### uncomment the 3 rows below for pm25 and energy electricity dataset, comment them for water stage dataset
+x = Dense(256, activation='relu', kernel_regularizer=keras.regularizers.L1L2(l1=1e-5, l2=1e-5))(x)
+x = Dense(128, activation='relu', kernel_regularizer=keras.regularizers.L1L2(l1=1e-5, l2=1e-5))(x)
+x = Dense(128, activation='relu', kernel_regularizer=keras.regularizers.L1L2(l1=1e-5, l2=1e-5))(x)
+x = Dense(train_y.shape[1])(x)
+
+model = Model(model_input, x)
+model.summary()
+
+
+# training
+lr = 0.00001
+EPOCHS = 8000
+
+lr_schedule = keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=5e-4, 
+                                                          decay_steps=10000,
+                                                          decay_rate=0.99)
+
+model.compile(
+#               optimizer=Adam(learning_rate=lr, decay=lr/EPOCHS), 
+              optimizer=Adam(learning_rate=lr_schedule),
+              loss='mse',
+              metrics=['mae'])
+
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=2, patience=1500)
+mc = ModelCheckpoint('saved_model/cnn1D_shifting_s{}_k{}_no_sc.h5'.format(S, K), monitor='val_mae', mode='min', verbose=2, save_best_only=True)
+
+history = model.fit(train_X, train_y,
+                    batch_size=512,
+                    epochs=EPOCHS,
+                    validation_data=(test_X, test_y),
+                    verbose=2,
+                    shuffle=True,
+                       callbacks=[es, mc])
+
+plt.rcParams["figure.figsize"] = (8, 6)
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel('Epoch', fontsize=16)
+plt.ylabel('Loss', fontsize=16)
+plt.legend(fontsize=14)
+plt.title("Training loss vs Testing loss", fontsize=18)
+# plt.savefig('graph/rnn_loss.png', dpi=300)
+plt.show()
+
+
+
+# Performance on test data
+saved_model = load_model('saved_model/cnn1D_shifting_s{}_k{}_no_sc.h5'.format(S, K))
+yhat = saved_model.predict(test_X)
+
+
+inv_yhat = scaler.inverse_transform(yhat)
+inv_y = scaler.inverse_transform(test_y)
+
+inv_yhat = pd.DataFrame(inv_yhat)
+inv_y = pd.DataFrame(inv_y)
+
+
+print('MAE = {}'.format(float("{:.4f}".format(mae(inv_yhat.iloc[:, :], inv_y.iloc[:, :])))))
+print('RMSE = {}'.format(float("{:.4f}".format(sqrt(mse(inv_yhat.iloc[:, :], inv_y.iloc[:, :]))))))
